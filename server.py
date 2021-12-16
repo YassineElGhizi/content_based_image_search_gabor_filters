9cf203d91e2396302cc04734bf527c95992badf8
Yassine EL GHIZI <yassine.ghizi2@gmail.com>
2021-12-16 22:32:29 +0100

    done

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
-from flask import Flask, redirect, url_for, render_template, request, flash , jsonify
+from flask import Flask, render_template, request , jsonify
 import time
 import os
-from skimage import io
 import cv2
-# from my_tools.train import train
 from my_tools.index import train
 from my_tools.index import train_one
-from my_tools.histogrameFeatures import HistogrameFeatures
 from my_tools.gabor import GaborDescriptor
 from my_tools.search import Search
 
 
 UPLOAD_FOLDER = 'static/images'
-hsv_init = (8,12,3)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -38,7 +34,6 @@ def upload():
     file = request.files['image']
     new_file_name = str(
         str(time.time()) + '.png'
-        # str(time.time()) + '.jpg'
     )
     file.save(os.path.join(
             app.config['UPLOAD_FOLDER'],new_file_name
@@ -47,13 +42,13 @@ def upload():
 
     train_one(str(UPLOAD_FOLDER + '/' + new_file_name))
 
-    # histFeatures = HistogrameFeatures(hsv_init)
+
 
     gd = GaborDescriptor(params)
     gaborKernels = gd.kernels()
     query = cv2.imread(os.path.join(os.path.dirname(__file__), 'static/images/' + new_file_name))
     features = gd.gaborHistogram(query, gaborKernels)
-    # features = histFeatures.features(query)
+
     #searching
     searcher = Search('./index.csv')
     results = searcher.search(features)
@@ -66,9 +61,7 @@ def upload():
     return jsonify(RESULTS_LIST)
 
 
-
-
-
+s
 
 if __name__ == '__main__':
     app.run(debug=True , host="0.0.0.0")
